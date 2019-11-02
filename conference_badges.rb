ab4cec0132d4773cbd91b40718092eb6762a62ec
Arslan Ali <ali.arslan319@gmail.com>
2019-11-02 00:52:21 -0400

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,13 +11,16 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees)
-  attendees.each_with_index.map do |attendee, index|
+  attendees.map.with_index do |attendee, index|
     "Hello, #{attendee}! You'll be assigned to room #{index + 1}!"
   end 
 end
 
 def printer(attendees)
-    assign_rooms(attendees).each do |message|
-  "Hello, #{attendee}! You'll be assigned to room #{index + 1}!"
-    end
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end 
+  assign_rooms(attendees).each do |room_assignment|
+    puts room_assignment
+  end 
 end
